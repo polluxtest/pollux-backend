75981a3b0c0625d53f91f83750469f315916c14e
gitName <a@b.c>
2022-05-28 07:56:50 -0500

    Update UsersController.cs

--- Pollux/Controllers/UsersController.cs
+++ Pollux/Controllers/UsersController.cs
@@ -73,11 +73,15 @@
             if (succeed != null)
             {
                 var token = await this.authService.SetAuth(loginModel);
-                this.logger.LogInformation("Cookie Domain :  " + this.cookieConfiguration.GetOptions().Domain);
+                this.logger.LogInformation("Cookie Domain :  " + 1);
                 this.HttpContext.Response.Cookies.Append(
                     CookiesConstants.CookieAccessTokenName,
                     token.AccessToken,
                     this.cookieConfiguration.GetOptions());
+
+                this.Response.Headers["Access-Control-Allow-Origin"] = this.cookieConfiguration.GetOptions().Domain;
+                this.Response.Headers["Access-Control-Allow-Credentials"] = 'true';
+
 
                 return this.Ok(succeed);
             }
